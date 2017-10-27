67d6abed861b7e21b18e9e2568a4b3ea08a7dedc
草原神兽 <wangjjwc@163.com>
2017-10-27 18:13:18 +0800

    冒泡排序算法

---
+++ src/main/java/com/wjj/BubbleSort.java
@@ -0,0 +1,30 @@
+package com.wjj;
+
+/**
+ * @author 作者：wangjj
+ * @version 创建时间：2017-10-2710:59
+ * @discription 类说明：冒泡排序
+ */
+public class BubbleSort {
+    public static void main(String[] args) {
+        //初始化长度为15的整数形数组
+        int[] array = {22, 4, 100, 3, 90, 1, 54, 77, 14, 66, 55, 24, 8, 10, 62};
+        //冒泡算法
+        //从小到大排序
+        for (int i = 0; i < array.length-1; i++) {
+            for (int j = 0; j < array.length-1 -i; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + "\t");
+        }
+    }
+
+}
